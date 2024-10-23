162549a1eb7fab162343ebbda09ddd2a48784aee
Muhammed Azhar <imuhammedazhar@icloud.com>
2024-10-23 21:07:39 +0100

    Solve exercise for Week 5.

--- STAT1050-SM/Week05/T5-Exercise.R
+++ STAT1050-SM/Week05/T5-Exercise.R
@@ -1,123 +1,94 @@
-# https://www.rdocumentation.org/packages/TimeSeries.OBeu/versions/1.2.4/topics/ts.stationary.test
-
-
 # Time Series Analysis: MA Models and Stationarity Tests
+# Source: https://www.rdocumentation.org/packages/TimeSeries.OBeu/versions/1.2.4/topics/ts.stationary.test
 
 ## 1. Simulating an MA(3) Process and Plotting Against a Normal Distribution
+
 # In this exercise, we will simulate an MA(3) process and compare it to a 
 # normal distribution.
 
-### R Code:
-
-# Load the necessary libraries
-if (!require(forecast)) install.packages("forecast")
+### Load necessary libraries
+if (!require(forecast)) install.packages("forecast", dependencies = TRUE)
 library(forecast)
 
-# Set the seed for reproducibility
+### Set the seed for reproducibility
 set.seed(123)
 
-# Simulate an MA(3) process with specific parameters
+### Simulate an MA(3) process with specific parameters
 ma_coeffs <- c(0.5, -0.3, 0.2)  # Coefficients for the MA process
 n <- 1000  # Number of observations
 ma_process <- arima.sim(n = n, list(ma = ma_coeffs))
 
-# Plot the simulated MA(3) process
+### Plot the simulated MA(3) process
 plot(ma_process, type = "l", main = "Simulated MA(3) Process", ylab = "y_t")
 
-# Plot the histogram of the simulated MA(3) process
-hist(ma_process, breaks = 30,
-     probability = TRUE,
+### Plot the histogram of the simulated MA(3) process
+hist(ma_process, breaks = 30, probability = TRUE,
      main = "Histogram of Simulated MA(3) Process", xlab = "Value")
 
-# Overlay a normal distribution on the histogram for comparison
-lines(density(ma_process), col = "red", lwd = 2)
-curve(dnorm(x, mean(ma_process),
-            sd(ma_process)), add = TRUE, col = "blue", lwd = 2)
+### Overlay a normal distribution on the histogram for comparison
+lines(density(ma_process), col = "red", lwd = 2)  # Kernel density estimate
+curve(dnorm(x, mean(ma_process), sd(ma_process)), add = TRUE, 
+      col = "blue", lwd = 2)  # Normal distribution
 legend("topright",
        legend = c("Density of MA(3)", "Normal Distribution"),
        col = c("red", "blue"), lwd = 2)
 
+## 2. Stationarity Tests
 
+### We know certain processes:
+# - White Noise (WN) process: stationary by default
+# - Random Walk (RW): non-stationary
+# - Differencing a RW: makes it stationary
+# - Adding a trend to WN: makes it trend-stationary
 
-# We want to test for stationarity
-# We may find ourselfs in a situation that we are not sure how to properly interpret the test output
-# We should first go to R Help (F1) and read the documentation
-# Then we can actually create data that we know whether or not are stationary and perform the tests
-# For example:
-# We know what a White Noise (WN) process is stationary (by default)
-# We know that a simple Random Walk (RW) is not stationary
-# We know that if we difference a simple RW then the process will become stationary
-# We know that if we add to a WN process a trend it will become Trend-Stationary (see below)
+### Create time series data for stationarity tests
+t <- 0:300
+y_stationary <- rnorm(length(t), mean = 1, sd = 1)  # Stationary time series
+y_trend <- cumsum(rnorm(length(t), mean = 1, sd = 4)) + t / 100  # Trend TS
 
+### Normalize the series for simplicity
+y_stationary <- y_stationary / max(y_stationary)
+y_trend <- y_trend / max(y_trend)
 
-# Practice on stationarity by testing on output that you know what to expect
-t = 0:300
-y_stationary <- rnorm(length(t),mean=1,sd=1) # the stationary time series (ts)
-y_trend      <- cumsum(rnorm(length(t),mean=1,sd=4))+t/100 # our ts with a trend
-# lets normalize each for simplicity (do you know what normalisation does)
-y_stationary<- y_stationary/max(y_stationary)
-y_trend      <- y_trend/max(y_trend)
+### Visualize the signals and their ACFs
+par(mfcol = c(2, 2))  # Set up a 2x2 plotting area
 
-# Second, we can check each for characteristics of stationarity
-# by looking at the autocorrelation functions (ACF) of each signal.
-# For a stationary signal, because we expect no dependence with time,
-# we would expect the ACF to go to 0 for each time lag (τ).
-# Lets visualize the signals and ACFs:
+# Plot the stationary signal and ACF
+plot(t, y_stationary, type = "l", col = "red", 
+     xlab = "Time (t)", ylab = "Y(t)", main = "Stationary Signal")
+acf(y_stationary, lag.max = length(y_stationary), main = "ACF of Stationary Signal")
 
-plot.new()
-frame()
-par(mfcol = c(2, 2))
-# the stationary signal and ACF
-plot(t, y_stationary,
-     type = "l", col = "red",
-     xlab = "time (t)",
-     ylab = "Y(t)",
-     main = "Stationary signal")
-acf(y_stationary, lag.max = length(y_stationary),
-    xlab = "lag #", ylab = "ACF", main = " ")
-# the trend signal and ACF
-plot(t, y_trend,
-     type = "l", col = "red",
-     xlab = "time (t)",
-     ylab = "Y(t)",
-     main = "Trend signal")
-acf(y_trend, lag.max = length(y_trend),
-    xlab = "lag #", ylab = "ACF", main = " ")
+# Plot the trend signal and ACF
+plot(t, y_trend, type = "l", col = "red", 
+     xlab = "Time (t)", ylab = "Y(t)", main = "Trend Signal")
+acf(y_trend, lag.max = length(y_trend), main = "ACF of Trend Signal")
 
-# Notably, the stationary signal (top left) results in few
-# significant lags that exceed the confidence interval of
-# the ACF (blue dashed line, bottom left) . In comparison,
-# the time series with a trend (top right) results in almost
-# all lags exceeding the confidence interval of the ACF (bottom right).
-# Qualitatively, we can see and conclude from the ACFs that the signal
-# on the left is stationary (due to the lags that die out) while the signal
-# on the right is not stationary (since later lags exceed the confidence
-# interval).
+### Observations:
+# - Stationary signal: ACF lags die out quickly.
+# - Trend signal: ACF lags persist, indicating non-stationarity.
 
-# Test on stationarity
+## 3. Stationarity Tests Using Augmented Dickey–Fuller (ADF) and KPSS Tests
+
+# Load the necessary library for stationarity tests
+if (!require(tseries)) install.packages("tseries", dependencies = TRUE)
 library(tseries)
-# A test we can conduct is the Augmented Dickey–Fuller (ADF)
-# t-statistic test to find if the series has a unit root
-# (a series with a trend line will have a unit root and result in a large p-value).
-# y_stationary (check the help function - you can test for different things)
-adf.test(y_stationary)
-adf.test(y_stationary, alternative = c("stationary"),k = trunc((length(y_stationary)-1)^(1/3)))
-adf.test(y_stationary, alternative = c("explosive"),k = trunc((length(y_stationary)-1)^(1/3)))
 
-# KPSS: a null hypothesis that an observable time series is stationary
-# We can test if the time series is level or trend
-# stationary using the Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test.
-# Here we will test the null hypothesis of trend stationarity
-# (a low p-value will indicate a signal that is not trend stationary, has a unit root):
-tseries::kpss.test(y_stationary, null = "Level")
-tseries::kpss.test(y_stationary)
-tseries::kpss.test(y_stationary, null = "Trend")
+### ADF Test: Test for a unit root (non-stationarity)
+# y_stationary tests
+adf.test(y_stationary)  # Default test
+adf.test(y_stationary, alternative = "stationary", k = trunc((length(y_stationary)-1)^(1/3)))  # Alternative hypothesis: stationary
+adf.test(y_stationary, alternative = "explosive", k = trunc((length(y_stationary)-1)^(1/3)))  # Alternative hypothesis: explosive
 
-# y_trend
-adf.test(y_trend, alternative = c("stationary"))
-adf.test(y_trend, alternative = c("explosive"))
-adf.test(y_trend)
+### KPSS Test: Null hypothesis of stationarity
+# Test for level and trend stationarity
+kpss.test(y_stationary, null = "Level")  # Test for level stationarity
+kpss.test(y_stationary, null = "Trend")  # Test for trend stationarity
 
-tseries::kpss.test(y_trend, null = "Level")
-tseries::kpss.test(y_trend)
-tseries::kpss.test(y_trend, null = "Trend")
+# y_trend tests
+adf.test(y_trend, alternative = "stationary")  # Test for stationary alternative
+adf.test(y_trend, alternative = "explosive")  # Test for explosive alternative
+adf.test(y_trend)  # Default test
+
+### KPSS Test on y_trend
+kpss.test(y_trend, null = "Level")  # Test for level stationarity
+kpss.test(y_trend, null = "Trend")  # Test for trend stationarity
