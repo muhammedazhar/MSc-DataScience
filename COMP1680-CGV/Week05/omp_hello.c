1f79396973ef9ec11891833bff969d0aa40bd288
Muhammed Azhar <imuhammedazhar@icloud.com>
2024-10-23 10:02:39 +0100

    Fix indentation in files.

--- COMP1680-CGV/Week05/omp_hello.c
+++ COMP1680-CGV/Week05/omp_hello.c
@@ -3,24 +3,22 @@
 #include <omp.h>
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int tid, nthreads;
     int THREADS = 2;
-    
-    if (argc==2){
+
+    if (argc==2) {
         THREADS=atoi(argv[1]);
     }
-    else{
-    printf("Number of threads expected defaulting to 2. \n");
+    else {
+        printf("Number of threads expected defaulting to 2. \n");
     }
-    
+
     omp_set_num_threads(THREADS);
-    
+
     // start of parallel section
     // Fork a team of threads with each thread having a private tid variable
-    #pragma omp parallel private(tid)
-    {
+    #pragma omp parallel private(tid) {
         tid=omp_get_thread_num();
         printf("Hello world from thread %d\n", tid);
         /* Only master thread does this */
@@ -28,11 +26,7 @@ int main(int argc, char** argv)
             nthreads = omp_get_num_threads();
             printf("Number of threads = %d\n", nthreads);
         }
-
-
     }//end of parallel section
     // All threads join master thread and terminate
-
-
     return 0;
 }  // end main()
