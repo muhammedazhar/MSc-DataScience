def9f6be371e3a94450da1d9c4d792da97ad811c
Muhammed Azhar <imuhammedazhar@icloud.com>
2024-11-24 04:55:37 +0000

    Refactor jacobi2d.c for improved memory management and segmentation fault.

--- COMP1680-CGV/Coursework/jacobi2d.c
+++ COMP1680-CGV/Coursework/jacobi2d.c
@@ -3,73 +3,84 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int m;
-    int n;
-    double tol;  // = 0.0001;
+    if (argc != 4) {
+        printf("Usage: %s m n tolerance\n", argv[0]);
+        return 1;
+    }
 
-    int i, j, iter;
-
-    m = atoi(argv[1]);
-    n = atoi(argv[2]);
-    tol = atof(argv[3]);
-
-    double t[m + 2][n + 2], tnew[m + 1][n + 1], diff, difmax;
+    int m = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    double tol = atof(argv[3]);
+    
+    // Dynamically allocate 2D arrays
+    double **t = (double **)malloc((m + 2) * sizeof(double *));
+    double **tnew = (double **)malloc((m + 2) * sizeof(double *));
+    
+    for (int i = 0; i < m + 2; i++) {
+        t[i] = (double *)malloc((n + 2) * sizeof(double));
+        tnew[i] = (double *)malloc((n + 2) * sizeof(double));
+    }
 
     printf("%d %d %lf\n", m, n, tol);
 
-    // initialise temperature array
-    for (i = 0; i <= m + 1; i++) {
-        for (j = 0; j <= n + 1; j++) {
+    // Initialize temperature array
+    for (int i = 0; i <= m + 1; i++) {
+        for (int j = 0; j <= n + 1; j++) {
             t[i][j] = 30.0;
         }
     }
 
-    // fix boundary conditions
-    for (i = 1; i <= m; i++) {
+    // Fix boundary conditions
+    for (int i = 1; i <= m; i++) {
         t[i][0] = 40.0;
         t[i][n + 1] = 90.0;
     }
-    for (j = 1; j <= n; j++) {
+    for (int j = 1; j <= n; j++) {
         t[0][j] = 30.0;
         t[m + 1][j] = 50.0;
     }
 
-    // main loop
-    iter = 0;
-    difmax = 1000000.0;
+    // Main loop
+    int iter = 0;
+    double difmax = 1000000.0;
+    
     while (difmax > tol) {
         iter++;
+        difmax = 0.0;
 
-        // update temperature for next iteration
-        for (i = 1; i <= m; i++) {
-            for (j = 1; j <= n; j++) {
-                tnew[i][j] =
-                    (t[i - 1][j] + t[i + 1][j] + t[i][j - 1] + t[i][j + 1]) /
-                    4.0;
+        // Update temperature for next iteration
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                tnew[i][j] = (t[i-1][j] + t[i+1][j] + t[i][j-1] + t[i][j+1]) / 4.0;
             }
         }
 
-        // work out maximum difference between old and new temperatures
-        difmax = 0.0;
-        for (i = 1; i <= m; i++) {
-            for (j = 1; j <= n; j++) {
-                diff = fabs(tnew[i][j] - t[i][j]);
-                if (diff > difmax) {
-                    difmax = diff;
-                }
-                // copy new to old temperatures
+        // Calculate maximum difference
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                double diff = fabs(tnew[i][j] - t[i][j]);
+                difmax = (diff > difmax) ? diff : difmax;
                 t[i][j] = tnew[i][j];
             }
         }
     }
 
-    // print results
-    printf("iter = %d  difmax = %9.11lf", iter, difmax);
-    for (i = 0; i <= m + 1; i++) {
-        printf("\n");
-        for (j = 0; j <= n + 1; j++) {
+    // Print results
+    printf("iter = %d  difmax = %9.11lf\n", iter, difmax);
+    for (int i = 0; i <= m + 1; i++) {
+        for (int j = 0; j <= n + 1; j++) {
             printf("%3.5lf ", t[i][j]);
         }
+        printf("\n");
     }
-    printf("\n");
+
+    // Free allocated memory
+    for (int i = 0; i < m + 2; i++) {
+        free(t[i]);
+        free(tnew[i]);
+    }
+    free(t);
+    free(tnew);
+
+    return 0;
 }
