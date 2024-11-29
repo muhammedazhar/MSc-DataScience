96568e74ae93dab73d4f0669cf822713a32b23d5
Muhammed Azhar <imuhammedazhar@icloud.com>
2024-11-29 12:37:58 +0000

    Refactor jacobi2d-Step1 script to compile with multiple optimization levels. Update testing loop to evaluate performance across different optimization settings and log results to a file.

--- COMP1680-CGV/Coursework/Solutions/jacobi2d-Step1.sh
+++ COMP1680-CGV/Coursework/Solutions/jacobi2d-Step1.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Compile the code with optimization
-echo -e "Compiling 'jacobi2d-Step2.c' OpenMP C program with C99 standard...\n"
-gcc -std=c99 -fopenmp jacobi2d-Step2.c -o jacobi2d-Step2
+echo -e "Compiling 'jacobi2d-Step1.c' program with C99 standard and different optimizations...\n"
+# Explicitly no optimization to avoid pontential overrides
+gcc -std=c99 -O0 jacobi2d-Step1.c -o jacobi2d-Step1-O0
+gcc -std=c99 -O1 jacobi2d-Step1.c -o jacobi2d-Step1-O1
+gcc -std=c99 -O2 jacobi2d-Step1.c -o jacobi2d-Step1-O2
+gcc -std=c99 -O3 jacobi2d-Step1.c -o jacobi2d-Step1-O3
 
 if [ $? -ne 0 ]; then
     echo "Compilation failed!"
@@ -15,13 +19,13 @@ echo "-----------------------------"
 # Loop over different matrix sizes
 for size in 150 200 250; do
     # Loop over different thread counts
-    for threads in 1 2 4 8; do
+    for optmlvl in 0 1 2 3; do
         # Print test configuration
-        echo "Testing grid size of ${size}x${size} with ${threads} threads"
-        # Run the program with specified size and threads
-        OMP_NUM_THREADS=$threads ./jacobi2d-Step2 $size $size 0.000100
+        echo "Testing grid size of ${size}x${size} with ${optmlvl} optimization level"
+        # Run the program with specified size and optimization level, redirecting output to a file
+        ./jacobi2d-Step1-$optmlvl $size $size 0.000100 >> jacobi2d-Step1.txt
         # Separator for readability
-        echo "--------------------------------------"
+        echo "--------------------------------------" >> jacobi2d-Step1.txt
     done
 done
 
